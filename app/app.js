996e21781b0533c79e2b71731417b49d0b3ca673
Ken Grønnbeck <kengroenn@gmail.com>
2014-03-01 20:56:31 +0100

    Connect before sending messages

--- app/app.js
+++ app/app.js
@@ -1,8 +1,7 @@
-
 var app = angular.module('irc-client', []);
 
 app.controller('LogController', ['$scope', function($scope) {
-	var websocket = new WebSocket('ws://localhost:1881');
+	var websocket = new WebSocket('ws://localhost:8080');
 
 	$scope.events = [];
 	websocket.onmessage = function(e) {
@@ -10,5 +9,24 @@ app.controller('LogController', ['$scope', function($scope) {
 		$scope.$apply(function() {
 			$scope.events.push(e);
 		});
+	};
+
+	$scope.connect = function() {
+		var connect = JSON.stringify({ 
+			type: 'connect', 
+			connection: {
+				server: 'irc.freenode.net', 
+				nick: 'tester-irc-proxy', 
+				channels: ['#nplol', '#pekkabot'] 
+			},
+			key: null
+		});
+
+		websocket.send(connect);
+	};
+
+	$scope.send = function(msg) {
+		var stringified = JSON.stringify({type: 'msg', to: '#nplol', 'payload': msg});
+		websocket.send(stringified);
 	};
 }]);
