b668ccc390d33c3984ae24b63385dacbdacad323
Ken Grønnbeck <kengroenn@gmail.com>
2014-04-21 21:44:18 +0200

    (func) Navigation updates the IRC model for chans

--- app/navigation.js
+++ app/navigation.js
@@ -2,18 +2,34 @@ app.directive('navigation', function() {
   return {
     restrict: 'E',
     templateUrl: 'templates/nav.html',
-    controller: function($scope, $rootScope) {
-      $scope.chans = []
+    controller: function($scope, $rootScope, api) {
 
       $scope.parseUri = function(uri) {
         return uri.replace('#', '%23')
       }
 
       $rootScope.$on('irc-add-channel', function(event, chan) {
-        if (!_.contains($scope.chans, chan)) {
-          $scope.chans.push(chan)
+        if (!_.contains($scope.model.windows, chan)) {
+          $scope.model.windows.push(chan)
         }
       })
+
+      api.all(function(all) {
+        $scope.model = _.first(all)
+        $scope.chans = $scope.model.windows
+      })
+
+      var save = function() {
+        api.insert($scope.model, function(model) {
+          console.log('saved model')
+        })
+      }
+      , lazySave = _.debounce(save, 300)
+
+      $scope.$watch('model', function(newVal, oldVal) {
+        if (oldVal == null) return
+        lazySave()
+      }, true)
     }
   }
 })
